9d2b39b7924fe0c98a887d60343dfcc49903c4b6
Zakareya Alatoli <zakareya.m.alatoli@gmail.com>
2023-02-27 13:41:58 -0500

    fix: vue-i18n plugin flags
    fix: i18n.global.locale returns a union of different strings. moment.locale() tries to call string.match() on this value but fails because i18n.global.locale is not a string but a union of strings. Now the value is used instead

--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,20 @@ module.exports = {
           maxSize: 4096
         }
       })
+    
+    config
+      .plugin('define')
+      .tap((definitions) => {
+        Object.assign(definitions[0], {
+          // ... rest of your injected vars here
+          // get rid of vue-i18 warning
+          __VUE_I18N_FULL_INSTALL__: JSON.stringify(true),
+          __INTLIFY_PROD_DEVTOOLS__: JSON.stringify(false),
+          __VUE_I18N_LEGACY_API__: JSON.stringify(false),
+        })
+  
+        return definitions
+      })
   },
 
   devServer: {
